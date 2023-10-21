42586c2605eea4b91679d84feb2ff02528743604
aurreland <aurelien.andreo@proton.me>
2023-10-21 13:21:52 +0200

    attachbelow

--- config.h
+++ config.h
@@ -95,6 +95,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY,			XK_Print,  spawn,	   {.v = (char *[]){ "/bin/sh", "-c", "shotgun -g $(hacksaw) | xclip -t 'image/png' -selection clipboard", NULL } } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
